d6d43cf94248e2a7460fe2d5e2bbbacbcd63ef59
kuangxianxi <kuangxianxi@taohuadaoinfo.com>
2020-03-25 20:53:47 +0800

    tabbar的封装,及跳转

---
+++ src/17-tabbar/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <main-tab-bar/>
+  </div>
+</template>
+
+<script>
+  import MainTabBar from "./components/tabbar/MainTabBar";
+  export default {
+    components: {
+      MainTabBar
+    }
+  }
+</script>
+
+<style>
+  @import "./assets/css/base.css";
+
+</style>
---
+++ src/17-tabbar/src/components/tabbar/MainTabBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <tab-bar>
+    <!--绑定的是变量就需要使用动态绑定,直接写字符串,就不需要动态绑定-->
+    <tab-bar-item path="/home">
+      <!--在dom中使用别名需要在前面加上~符号-->
+      <img src="~assets/img/tabbar/home.svg" alt="" slot="item-icon">
+      <img src="~assets/img/tabbar/home_active.svg" alt="" slot="item-icon-active">
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item path="/category">
+      <img src="~assets/img/tabbar/category.svg" alt="" slot="item-icon">
+      <img src="~assets/img/tabbar/category_active.svg" alt="" slot="item-icon-active">
+      <div slot="item-text">分类</div>
+    </tab-bar-item>
+    <tab-bar-item path="/cart">
+      <img src="~assets/img/tabbar/cart.svg" alt="" slot="item-icon">
+      <img src="~assets/img/tabbar/cart_active.svg" alt="" slot="item-icon-active">
+      <div slot="item-text">购物车</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile">
+      <img src="~assets/img/tabbar/profile.svg" alt="" slot="item-icon">
+      <img src="~assets/img/tabbar/profile_active.svg" alt="" slot="item-icon-active">
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+  import TabBar from "./TabBar";
+  import TabBarItem from "./TabBarItem";
+  export default {
+    name: "MainTabBar",
+    components: {
+      TabBarItem,
+      TabBar
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
